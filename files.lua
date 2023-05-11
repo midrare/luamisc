7f557c4efba457346d6db0bb2971d2d1b28713d3
midrare <midrare9@gmail.com>
2023-05-10 22:49:29 -0700

    fix: shims

--- files.lua
+++ files.lua
@@ -1,4 +1,4 @@
 local absname, _ = ...
-local basename = absname:match('([^\\.]+)$')
-local parent = absname:gsub('\\.[^\\.]+$', '')
+local basename = absname:match('([^%.]+)$')
+local parent = absname:gsub('%.[^%.]+$', '')
 return require(parent .. '.lua.luamisc.' .. basename)
