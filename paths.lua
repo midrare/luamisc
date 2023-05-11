adc4d4677c876b5a7bb9640aae74fecc2577c7ac
midrare <midrare9@gmail.com>
2023-05-11 14:23:01 -0700

    refactor: no shims

--- paths.lua
+++ paths.lua
@@ -1,4 +1,170 @@
-local absname, _ = ...
-local basename = absname:match('([^%.]+)$')
-local parent = absname:gsub('%.[^%.]+$', '')
-return require(parent .. '.lua.luamisc.' .. basename)
+local modulename, _ = ...
+---@diagnostic disable-next-line: unused-local
+local moduleroot = modulename:gsub("(.+)%..+", "%1")
+
+local module = {}
+
+local is_windows = vim.fn.has('win32') > 0
+local path_sep = vim.fn.has('win32') > 0 and '\\' or '/'
+
+
+---@return string sep os-specific path separator
+---@nodiscard
+function module.sep()
+  return path_sep
+end
+
+---@param filename string file path
+---@return string? basename last component of file path
+---@nodiscard
+function module.basename(filename)
+  local bname, _ = filename:gsub('^.*[\\/](.+)[\\/]*', '%1')
+  if not bname or #bname <= 0 then
+    return nil
+  end
+  return bname
+end
+
+---@param filename string file path
+---@return string? dir parent directory of path
+---@nodiscard
+function module.dirname(filename)
+  local parent_dir = filename:match('^(.*[\\/]).+$')
+  if not parent_dir or #parent_dir <= 0 then
+    return nil
+  end
+  return parent_dir:match('^(.+)[\\/]+$') or parent_dir
+end
+
+---@param filename string file path
+---@return string? stem base name without file extension
+---@nodiscard
+function module.filestem(filename)
+  local basename = filename:match('^.+[\\/](.+)$') or filename
+  local stem = basename:gsub('^(.+)%.[^%s]+$', '%1')
+  if not stem or #stem <= 0 then
+    return nil
+  end
+  return stem
+end
+
+---@param filename string file path
+---@return string? file extension if any
+---@nodiscard
+function module.fileext(filename)
+  local basename = filename:match('^.+[\\/](.+)$') or filename
+  local ext = basename:match('^.+(%.[^%s]+)$')
+  if not ext or #ext <= 0 then
+    return nil
+  end
+end
+
+---@param filename string file path
+---@return string filepath file path with path separators for current os
+---@nodiscard
+function module.normcase(filename)
+  local p, _ = nil, nil
+  if is_windows then
+    p, _ = filename:gsub('/', '\\')
+  else
+    p, _ = filename:gsub('\\', '/')
+  end
+  return p
+end
+
+---@param filename string file path
+---@return string filepath file path with ".." collapsed
+---@nodiscard
+function module.normpath(filename)
+  if is_windows then
+    if filename:match('^\\\\') then -- UNC
+      return '\\\\' .. module.normpath(filename:sub(3))
+    end
+    filename = filename:gsub('/', '\\')
+  end
+
+  local num_subs = 0
+  repeat
+    -- // to /
+    filename, num_subs = filename:gsub('[\\/][\\/]+', path_sep)
+  until num_subs <= 0
+
+  repeat
+    -- /./ to /
+    filename, num_subs = filename:gsub('[\\/]%.[\\/]', path_sep)
+  until num_subs <= 0
+
+  repeat
+    -- foo/.. to ""
+    filename, num_subs = filename:gsub('[^\\/]+[\\/]%.%.[\\/]*', '')
+  until num_subs <= 0
+
+  if filename == '' then
+    filename = '.'
+  end
+
+  -- foo/bar/ to foo/bar
+  filename = filename:gsub('[\\/]+$', '')
+
+  -- C: to C:\
+  if is_windows and filename:match('^[^\\/]+:$') then
+    filename = filename .. '\\'
+  end
+
+  return filename
+end
+
+---@param filename string file path
+---@return boolean is_abs if file path is an absolute path
+---@nodiscard
+function module.isabs(filename)
+  return filename:match('^[\\/]') or filename:match('^[a-zA-Z]:[\\/]')
+end
+
+---@param filename string file path
+---@param pwd string current directory
+---@return string filename absolute file path
+---@nodiscard
+function module.abspath(filename, pwd)
+  filename = filename:gsub('[\\/]+$', '')
+  if not module.isabs(filename) then
+    filename = pwd:gsub('[\\/]+$', '') .. path_sep .. filename
+  end
+  return module.normpath(filename)
+end
+
+---@param filename string file path
+---@param cwd string current directory
+---@return string filename canonical file path
+---@nodiscard
+function module.canonical(filename, cwd)
+  local normcased = module.normcase(filename)
+
+  if not module.isabs(normcased) then
+    return module.abspath(normcased, cwd)
+  end
+
+  return normcased
+end
+
+---@vararg string file path segments
+---@return string filepath file path joined using os-specific path separator
+---@nodiscard
+function module.join(...)
+  local sep = module.sep()
+  local joined = ''
+
+  for i = 1, select('#', ...) do
+    local el = select(i, ...):gsub('[\\/]+$', '')
+    if el and #el > 0 then
+      if #joined > 0 then
+        joined = joined .. sep
+      end
+      joined = joined .. el
+    end
+  end
+
+  return joined
+end
+
+return module
--- tables.lua
+++ tables.lua
@@ -1,4 +1,161 @@
-local absname, _ = ...
-local basename = absname:match('([^%.]+)$')
-local parent = absname:gsub('%.[^%.]+$', '')
-return require(parent .. '.lua.luamisc.' .. basename)
+local modulename, _ = ...
+---@diagnostic disable-next-line: unused-local
+local moduleroot = modulename:gsub("(.+)%..+", "%1")
+
+local module = {}
+
+local function is_array(o)
+  for key, _ in pairs(o) do
+    if type(key) ~= 'number' then
+      return false
+    end
+  end
+
+  return true
+end
+
+
+function module.clone(tbl, deep)
+  local target = {}
+
+  for k, v in pairs(tbl) do
+    if deep and type(v) == 'table' then
+      target[k] = module.clone(v, deep)
+    else
+      target[k] = v
+    end
+  end
+
+  local meta = getmetatable(tbl)
+  setmetatable(target, not deep and meta or module.clone(meta, deep))
+
+  return target
+end
+
+
+function module.merge(dest, src, opts)
+  assert(type(dest) == 'table', 'destination must be a table (array ok)')
+  assert(src == nil or type(src) == 'table', 'source must be a table (nil ok)')
+
+  opts = opts or {}
+
+  if src ~= nil then
+    if is_array(src) and is_array(dest) then
+      for _, value in ipairs(src) do
+        if not opts.unique or not vim.tbl_contains(dest, value) then
+          table.insert(dest, value)
+        end
+      end
+    else
+      for key, value in pairs(src) do
+        if type(value) == 'table' and type(dest[key]) == 'table' then
+          module.merge(dest[key], value)
+        elseif dest[key] == nil or (opts.force == nil or opts.force) then
+          dest[key] = value
+        end
+      end
+    end
+  end
+end
+
+
+function module.contains(tbl, value)
+  if tbl ~= nil then
+    for _, v in ipairs(tbl) do
+      if v == value then
+        return true
+      end
+    end
+  end
+  return false
+end
+
+
+function module.is_empty(tbl)
+  return next(tbl) == nil
+end
+
+
+function module.make_keys(tbl, ...)
+  local current = tbl
+  for i = 1, select('#', ...) do
+    local key = select(i, ...)
+    if current[key] == nil then
+      current[key] = {}
+    end
+    current = current[key]
+  end
+end
+
+
+function module.has_keys(tbl, ...)
+  if tbl == nil then
+    return false
+  end
+
+  local current = tbl
+  for i = 1, select('#', ...) do
+    local key = select(i, ...)
+    if current[key] == nil then
+      return false
+    end
+    current = current[key]
+  end
+
+  return true
+end
+
+
+local function overwrite(src, target)
+  for k, v in pairs(src) do
+    if type(v) == 'table' then
+      target[k] = {}
+      overwrite(v, target[k])
+    else
+      target[k] = v
+    end
+  end
+end
+
+
+function module.overwrite(src, target)
+  for k, _ in pairs(target) do
+    target[k] = nil
+  end
+  overwrite(src, target)
+end
+
+
+function module.tostring(o)
+  local type_ = type(o)
+  if type_ == 'string' then
+    return '"' .. tostring(o) .. '"'
+  elseif type_ ~= 'table' then
+    return tostring(o)
+  end
+
+  local is_first = true
+  local is_array_ = is_array(o)
+
+  local result = is_array_ and '[' or '{'
+  for key, value in pairs(o) do
+    if not is_first then
+      result = result .. ', '
+    end
+
+    if not is_array_ then
+      if type(key) == 'string' and key:find('%s') then
+        result = result .. '"' .. key .. '"' .. ': '
+      else
+        result = result .. tostring(key) .. ': '
+      end
+    end
+
+    result = result .. module.tostring(value)
+    is_first = false
+  end
+
+  return result .. (is_array_ and ']' or '}')
+end
+
+return module
