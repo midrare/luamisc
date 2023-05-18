5e3cfbb7a9c1bfe4a4b6ee5a2888158da20cd901
midrare <midrare9@gmail.com>
2023-05-17 22:31:48 -0700

    test: fix get_script_dir()

--- tests/test_platform.lua
+++ tests/test_platform.lua
@@ -17,7 +17,13 @@ local function get_script_dir(source)
   if pipe then
     pipe:close()
   end
-  return cwd .. "/" .. script:gsub("[\\/]+[^\\/]*$", "")
+
+  local dir = cwd or "."
+  if script:match("[\\/][^\\/]+$") then
+    dir = dir .. "/" .. script:gsub("[\\/]+[^\\/]*$", "")
+  end
+
+  return dir
 end
 
 package.path = package.path
